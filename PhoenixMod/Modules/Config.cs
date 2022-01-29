fc4db09346c21043568ea4aab853805ca1904089
Justin Hoch <justinhoch22@gmail.com>
2022-01-28 20:15:47 -0600

    Added a config option for turnabout stacks

--- PhoenixMod/Modules/Config.cs
+++ PhoenixMod/Modules/Config.cs
@@ -7,9 +7,11 @@ namespace PhoenixWright.Modules
     public static class Config
     {
         public static ConfigEntry<bool> loweredVolume;
+        public static ConfigEntry<bool> gainTurnaboutOnHit;
         public static void ReadConfig()
         {
             loweredVolume = PhoenixPlugin.instance.Config.Bind<bool>(new ConfigDefinition("Sound settings", "SFX"), false, new ConfigDescription("Set to true to lower SFX"));
+            gainTurnaboutOnHit = PhoenixPlugin.instance.Config.Bind<bool>(new ConfigDefinition("Turnabout stacks", "Turnabout stacks"), false, new ConfigDescription("Set to true to enable gain turnabout on hit"));
         }
 
         // this helper automatically makes config entries for disabling survivors
